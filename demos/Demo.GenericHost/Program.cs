8d9825b57f73c6c1ff1c3ad929353f687991466c
Dante De Ruwe <dante.deruwe@ae.be>
2024-11-19 21:38:51 +0100

    Generichost cleanup

--- demos/Demo.GenericHost/Program.cs
+++ demos/Demo.GenericHost/Program.cs
@@ -4,10 +4,7 @@ using Microsoft.Extensions.Logging;
 
 var builder = Host.CreateDefaultBuilder(args);
 
-builder.ConfigureLogging(logging =>
-{
-    logging.AddFilter("Microsoft", LogLevel.Warning);
-});
+builder.ConfigureLogging(logging => { logging.AddFilter("Microsoft", LogLevel.Warning); });
 
 builder.ConfigureServices(services =>
 {
@@ -53,45 +50,7 @@ file class ConsoleHostedService(string[] args, IHostApplicationLifetime appLifet
                 return 0;
             }
             case ["generate", "number", .. var options]:
-            {
-                int from = 0, to = 100;
-
-                // Parse '--from' argument
-                if (options.Contains("--from"))
-                {
-                    var index = Array.IndexOf(options, "--from");
-                    var hasFrom = index + 1 < options.Length && int.TryParse(options[index + 1], out from);
-                    if (!hasFrom)
-                    {
-                        Console.WriteLine("Invalid value for --from.");
-                        return 1;
-                    }
-                }
-
-                // Parse '--to' argument
-                if (options.Contains("--to"))
-                {
-                    var index = Array.IndexOf(options, "--to");
-                    var hasTo = index + 1 < options.Length && int.TryParse(options[index + 1], out to);
-                    if (!hasTo)
-                    {
-                        Console.WriteLine("Invalid value for --to.");
-                        return 1;
-                    }
-                }
-
-                // Validate if 'from' is greater than 'to'
-                if (from > to)
-                {
-                    Console.WriteLine("The 'from' value cannot be greater than the 'to' value.");
-                    return 1;
-                }
-
-                // Generate a random number in the specified range
-                var number = Random.Shared.Next(from, to + 1); // The upper bound is exclusive, so add +1 to include 'to'
-                Console.WriteLine($"Generated number: {number}");
-                return 0;
-            }
+                return ExecuteGenerateNumberCommand(options);
             case ["generate", ..]:
                 Console.WriteLine("Invalid command. Use 'guid' or 'number'.");
                 return 1;
@@ -99,5 +58,46 @@ file class ConsoleHostedService(string[] args, IHostApplicationLifetime appLifet
                 Console.WriteLine("Unknown command.");
                 return 1;
         }
+    }
+
+    private static int ExecuteGenerateNumberCommand(string[] options)
+    {
+        int from = 0, to = 100;
+
+        // Parse '--from' argument
+        if (options.Contains("--from"))
+        {
+            var index = Array.IndexOf(options, "--from");
+            var hasFrom = index + 1 < options.Length && int.TryParse(options[index + 1], out from);
+            if (!hasFrom)
+            {
+                Console.WriteLine("Invalid value for --from.");
+                return 1;
+            }
+        }
+
+        // Parse '--to' argument
+        if (options.Contains("--to"))
+        {
+            var index = Array.IndexOf(options, "--to");
+            var hasTo = index + 1 < options.Length && int.TryParse(options[index + 1], out to);
+            if (!hasTo)
+            {
+                Console.WriteLine("Invalid value for --to.");
+                return 1;
+            }
+        }
+
+        // Validate if 'from' is greater than 'to'
+        if (from > to)
+        {
+            Console.WriteLine("The 'from' value cannot be greater than the 'to' value.");
+            return 1;
+        }
+
+        // Generate a random number in the specified range
+        var number = Random.Shared.Next(from, to + 1); // The upper bound is exclusive, so add +1 to include 'to'
+        Console.WriteLine($"Generated number: {number}");
+        return 0;
     }
 }
