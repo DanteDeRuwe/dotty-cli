06e8bf49d212566a65b9192090197515225a9983
Dante De Ruwe <dante.deruwe@ae.be>
2024-11-19 19:03:23 +0100

    Clean up code

--- src/Dotty.CLI/Commands/SimpleCommands.cs
+++ src/Dotty.CLI/Commands/SimpleCommands.cs
@@ -1,8 +1,6 @@
 using Cocona.Application;
 using Cocona.Help;
-using Cocona.Help.DocumentModel;
 using Spectre.Console;
-using Spectre.Console.Rendering;
 
 namespace Dotty.CLI.Commands;
 
@@ -13,33 +11,14 @@ public class SimpleCommands : ICommandDefinition
         app.AddCommand("greet", ([Argument] string subject) => Panel($"Hello, {subject}!"));
 
         app.AddCommand(([FromService] ICoconaApplicationMetadataProvider metadataProvider, [FromService] ICoconaHelpMessageBuilder helpProvider) =>
-            {
-                Panel(new Rows(
-                    new FigletText("dotty").Color(Color.Aqua).Centered(),
-                    new Markup($"[bold][aqua]{metadataProvider.GetVersion()}[/][/]").Centered(),
-                    new Rule { Style = Style.Parse("gray") },
-                    new Text("\n"),
-                    new Text(helpProvider.BuildAndRenderForCurrentCommand())
-                ));
-            })
-            .OptionLikeCommand(x =>
-            {
-                x.Add("tree", () => Tree("Dotty CLI", t => t
-                    .WithChild("greet")
-                    .WithChild("convert", c => c
-                        .WithChild("units")
-                        .WithChild("tobase64")
-                        .WithChild("frombase64")
-                    )
-                    .WithChild("generate", g => g
-                        .WithChild("random", r => r
-                            .WithChild("guid")
-                            .WithChild("number")
-                            .WithChild("fromtemplate")
-                        )
-                        .WithChild("timestamp")
-                    )
-                ));
-            });
+        {
+            Panel(new Rows(
+                new FigletText("dotty").Color(Color.Aqua).Centered(),
+                new Markup($"[bold][aqua]{metadataProvider.GetVersion()}[/][/]").Centered(),
+                new Rule { Style = Style.Parse("gray") },
+                new Text("\n"),
+                new Text(helpProvider.BuildAndRenderForCurrentCommand())
+            ));
+        });
     }
 }
