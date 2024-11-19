9843d553e8a9dfbfc762f97efcd3d3a566d95e97
Dante De Ruwe <dante.deruwe@ae.be>
2024-11-19 20:15:32 +0100

    Add weather commands

--- src/Dotty.CLI/Commands/WeatherCommands.cs
+++ src/Dotty.CLI/Commands/WeatherCommands.cs
@@ -1,8 +1,89 @@
+using System.Globalization;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+using Spectre.Console;
+
 namespace Dotty.CLI.Commands;
 
-public class WeatherCommands: ICommandDefinition
+public class WeatherCommands : ICommandDefinition
 {
     public void Register(ICoconaAppBuilder app)
     {
+        app.AddCommand("weather", ExecuteWeatherCommand);
+    }
+
+    private static async Task ExecuteWeatherCommand([FromService] IWeatherService weatherService, [Argument] string city)
+    {
+        var weather = await weatherService.GetWeather(city);
+        if (weather is null)
+        {
+            Error($"Could not find weather for {city}");
+            return;
+        }
+
+        var dailyWeatherAtNoon = Enumerable
+            .Zip(weather.Hourly.Time, weather.Hourly.Temperature, weather.Hourly.Humidity)
+            .Select((values, i) => new { Time = values.Item1, Temperature = values.Item2, Humidity = values.Item3 })
+            .GroupBy(x => x.Time.Hour)
+            .Where(g => g.Key == 12)
+            .SelectMany(g => g);
+
+        var table = new Table().AddColumns("Time", "Temperature", "Humidity").Centered();
+        AnsiConsole.Live(table).Start(ctx =>
+        {
+            foreach (var w in dailyWeatherAtNoon)
+            {
+                table.AddRow(new TableRow([
+                    new Markup($"[bold]{w.Time:dd MMMM}[/]"),
+                    new Text($"{w.Temperature:F1}°C"),
+                    new Text($"{w.Humidity:F1}%")
+                ]));
+                ctx.Refresh();
+                Thread.Sleep(250); // Simulate slower rendering
+            }
+        });
     }
 }
+
+public interface IWeatherService
+{
+    Task<Weather?> GetWeather(string city);
+}
+
+public class WeatherService(IHttpClientFactory clientFactory) : IWeatherService
+{
+    public async Task<Weather?> GetWeather(string city)
+    {
+        using var cityClient = clientFactory.CreateClient("CityClient");
+        var cityResponse = await cityClient.GetFromJsonAsync<CityResponse>($"/v1/search?name={city}&count=1&language=en&format=json");
+        if (cityResponse is not { Results: { Length: >= 0 } cityResults }) return null;
+
+        var cities = cityResults.Select(r => (r.Latitude, r.Longitude)).ToArray();
+        var (lat, lon) = cities.First();
+
+        using var weatherClient = clientFactory.CreateClient("WeatherClient");
+        var response = await weatherClient.GetFromJsonAsync<Weather>($"/v1/forecast?latitude={lat.ToString("F", CultureInfo.InvariantCulture)}&longitude={lon.ToString("F", CultureInfo.InvariantCulture)}&hourly=temperature_2m,relative_humidity_2m");
+
+        return response;
+    }
+}
+
+file record CityResponse(
+    [property: JsonPropertyName("results")]
+    CityResults[] Results);
+
+file record CityResults(
+    [property: JsonPropertyName("latitude")]
+    double Latitude,
+    [property: JsonPropertyName("longitude")]
+    double Longitude);
+
+public record Hourly(
+    [property: JsonPropertyName("time")] DateTime[] Time,
+    [property: JsonPropertyName("temperature_2m")]
+    double[] Temperature,
+    [property: JsonPropertyName("relative_humidity_2m")]
+    double[] Humidity);
+
+public record Weather(
+    [property: JsonPropertyName("hourly")] Hourly Hourly);
--- src/Dotty.CLI/Program.cs
+++ src/Dotty.CLI/Program.cs
@@ -1,10 +1,33 @@
 using Cocona.Application;
+using Dotty.CLI.Commands;
 using Dotty.CLI.Helpers;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 var builder = CoconaApp.CreateBuilder();
 
 builder.Services.AddSingleton<ICoconaApplicationMetadataProvider, CustomMetadataProvider>();
+
+// Restrict logging for  System.Net.Http.HttpClient to warning level
+builder.Services.AddLogging(logging =>
+{
+    logging.AddFilter("System.Net.Http.HttpClient", LogLevel.Warning);
+});
+
+builder.Services.AddHttpClient("WeatherClient", client =>
+{
+    client.BaseAddress = new Uri("https://api.open-meteo.com/");
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
+});
+
+builder.Services.AddHttpClient("CityClient", client =>
+{
+    client.BaseAddress = new Uri("https://geocoding-api.open-meteo.com/");
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
+});
+
+builder.Services.AddSingleton<IWeatherService, WeatherService>();
+
 
 var app = builder.Build();
 
