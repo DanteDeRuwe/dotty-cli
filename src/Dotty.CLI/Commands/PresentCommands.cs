4546d2151e19b2fb3872deb0a88d20a07d59d076
Dante De Ruwe <dante.deruwe@ae.be>
2024-11-20 11:19:50 +0100

    Fix slides link to public url

--- src/Dotty.CLI/Commands/PresentCommands.cs
+++ src/Dotty.CLI/Commands/PresentCommands.cs
@@ -12,7 +12,7 @@ public class PresentCommands : ICommandDefinition
             {
                 Process.Start(new ProcessStartInfo
                 {
-                    FileName = "https://docs.google.com/presentation/d/1WABgifl2J70RuZjVP8MJkAf4k5TWAohhuNUtBViE5Fs/present",
+                    FileName = "https://docs.google.com/presentation/d/e/2PACX-1vQsfvb5kOIzYOGehsM9y88kjP_Xl8_T1PsD6lPosESupl8EwK9hVxmLmjomTfnjlQ3lwiXop2noJBEe/pub?start=true&loop=false&delayms=3000",
                     UseShellExecute = true
                 });
             });
